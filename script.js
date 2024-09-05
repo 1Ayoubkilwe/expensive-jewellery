4cc36efa44f0bf301827b8d8495bd7ba92f276c9
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-05 11:11:52 +0300

    answer

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Good Luck 💪🏾
+function mostExpensive(jewelry) {
+    let mostExpensiveItem = "";
+    let maxPrice = 0;
+  
+    for (let item in jewelry) {
+      if (jewelry[item] > maxPrice) {
+        maxPrice = jewelry[item];
+        mostExpensiveItem = item;
+      }
+    }
+  
+    return `The most expensive one is the ${mostExpensiveItem}`;
+  }
+  
+  // Test cases
+  console.log(mostExpensive({
+    "Diamond Earrings": 980,
+    "Gold Watch": 250,
+    "Pearl Necklace": 4650
+  })); 
+  // ➞ "The most expensive one is the Pearl Necklace"
+  
+  console.log(mostExpensive({
+    "Silver Bracelet": 280,
+    "Gemstone Earrings": 180,
+    "Diamond Ring": 3500
+  }));
+  // ➞ "The most expensive one is the Diamond Ring"
+  
